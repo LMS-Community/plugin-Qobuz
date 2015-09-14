7f48dc4a6962b4b63db1dbc050faed2cc80b2bb8
michaelherger <michael@herger.net>
2015-09-14 19:10:03 +0200

    Use curl instead of wget, as it's installed on OSX by default.

--- build.sh
+++ build.sh
@@ -27,7 +27,7 @@ rm -rf Qobuz/
 
 SHA=`shasum $ZIPFILE`
 
-wget --no-check-certificate -q -O repo.xml http://www.herger.net/slim-plugins/qobuz-repo.xml
+curl -o repo.xml http://www.herger.net/slim-plugins/qobuz-repo.xml
 #wget --no-check-certificate -q http://www.pierrebeck.fr/SqueezeboxQobuz/repo.xml
 
 if [ -e repo.xml ]; then
