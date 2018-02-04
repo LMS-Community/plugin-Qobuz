c5c0bc25ce8b309729c1442f3635254057d00bfa
marcoc1712 <marcoc1712@gmail.com>
2018-02-04 04:06:37 +0100

    Patch Qobuz plugin, mods to ProtocolHandler as per MH requests

--- ProtocolHandler.pm
+++ ProtocolHandler.pm
@@ -7,7 +7,7 @@ use base qw(Slim::Player::Protocols::HTTP);
 use Scalar::Util qw(blessed);
 
 use Slim::Utils::Log;
-use Slim::Utils::Misc;
+#use Slim::Utils::Misc;
 use Slim::Utils::Prefs;
 
 use List::Util qw(min max first);
@@ -15,28 +15,28 @@ use Slim::Utils::Errno;
 
 # Size of the _sysread chunksize AND buffers, NOT to be confuesed with same applied to pipeline and/or player.
 
-use constant MAX_OUT    => 512*1024*1024; # safety limit, the out should always be empty, if reached _sysread stops reading from source.
+use constant MAX_OUT => 512*1024*1024; # safety limit, the out should always be empty, if reached _sysread stops reading from source.
 
-use constant MIN_OUT    => 256*1024;      # data are not transferred to output until this limit is reached in input.
-use constant RANGE_SIZE => 1024*1024;     # size of range in the http get request, not really the chunksize, but similar.
+use constant MIN_OUT => 256*1024; # data are not transferred to output until this limit is reached in input.
+use constant RANGE_SIZE => 1024*1024; # size of range in the http get request, not really the chunksize, but similar.
 
 # Note that playback will not start untill the firs http get returns, so the greather value between RANGE_SIZE and MIN_OUT, is the real 
 # threshold.
 
 # Seconds of delay before playback starts (initial buffering seconds)
-use constant BUFFERING_SECONDS  => 5;
+use constant BUFFERING_SECONDS => 5;
 
 use Plugins::Qobuz::API;
 
-my $log   = logger('plugin.qobuz');
+my $log = logger('plugin.qobuz');
 my $prefs = preferences('plugin.qobuz');
 
 sub new {
-	my $class  = shift;
-	my $args   = shift;
+	my $class = shift;
+	my $args = shift;
 
-	my $client    = $args->{client};
-	my $song      = $args->{song};
+	my $client = $args->{client};
+	my $song = $args->{song};
 	my $streamUrl = $song->streamUrl() || return;
 	
 	main::DEBUGLOG && $log->is_debug && $log->debug( 'Remote streaming Qobuz track: ' . $streamUrl );
@@ -44,24 +44,24 @@ sub new {
 	my $mime = $song->pluginData('mime');
 
 	my $sock = $class->SUPER::new( {
-		url     => $streamUrl,
-		song    => $song,
-		client  => $client,
-#		bitrate => $mime =~ /flac/i ? 750_000 : 320_000,
+		url => $streamUrl,
+		song => $song,
+		client => $client,
+		#bitrate => $mime =~ /flac/i ? 750_000 : 320_000,
 	} ) || return;
 	
 	${*$sock}{contentType} = $mime;
 
-    if (defined($sock)) {
-		${*$sock}{'vars'} = {                   # variables which hold state for this instance:
-			'inBuf'       => '',                # buffer of received data
-			'outBuf'      => '',                # buffer of processed audio
-			'offset'      => 0,                 # offset for next HTTP request
-			'streaming'   => 1,                 # flag for streaming, changes to 0 when all data received
-			'fetching'    => 0,                 # waiting for HTTP data
+	if (defined($sock)) {
+		${*$sock}{'vars'} = { # variables which hold state for this instance:
+			'inBuf' => '', # buffer of received data
+			'outBuf' => '',# buffer of processed audio
+			'offset' => 0, # offset for next HTTP request
+			'streaming' => 1, # flag for streaming, changes to 0 when all data received
+			'fetching' => 0, # waiting for HTTP data
 		};
-    }
-    return $sock;
+	}
+	return $sock;
 }
 
 sub vars {
@@ -78,50 +78,50 @@ sub vars {
 #
 
 sub bufferThreshold{
-	my ($class, $client, $url) = @_;
+my ($class, $client, $url) = @_;
 
-    #We have bufferThreshold in prefs, let's use it.
-    my $clientPrefs= $prefs->client($client);
-    my $bufferThreshold= $clientPrefs->get('bufferThreshold');
-    
-    my $bufferSecs = $prefs->get('bufferSecs') || BUFFERING_SECONDS;
-    #limit to BUFFERING_SECONDS seconds.
-    if ($bufferSecs > BUFFERING_SECONDS){$bufferSecs = BUFFERING_SECONDS;}
-    
-    my $format = $class->getFormatForURL($url);
-    
-	return ($format eq 'flc' ? 80 : 32) * $bufferSecs;
+#We have bufferThreshold in prefs, let's use it.
+my $clientPrefs= $prefs->client($client);
+
+my $bufferThreshold= $clientPrefs->get('bufferThreshold');
+
+if ($bufferThreshold){return $bufferThreshold};
+
+my $bufferSecs = $prefs->get('bufferSecs') || BUFFERING_SECONDS;
+#limit to BUFFERING_SECONDS seconds.
+if ($bufferSecs > BUFFERING_SECONDS){$bufferSecs = BUFFERING_SECONDS;}
+
+my $format = $class->getFormatForURL($url);
+
+return ($format eq 'flc' ? 80 : 32) * $bufferSecs;
+
 }
 
 sub sysread {
 	my $self = $_[0];
-    #my $chunk = $_[1];
+	#my $chunk = $_[1];
 	my $chunkSize = $_[2];
 
 	my $metaInterval = ${*$self}{'metaInterval'};
-	my $metaPointer  = ${*$self}{'metaPointer'};
-    
-    #Data::Dump::dump("Plugins::Qobuz::ProtocolHandler - sysread: ", $metaInterval, $metaPointer, $chunkSize);
-    my $readLength;
-    
+	my $metaPointer = ${*$self}{'metaPointer'};
+ 
+	#Data::Dump::dump("Plugins::Qobuz::ProtocolHandler - sysread: ", $metaInterval, $metaPointer, $chunkSize);
+	my $readLength;
+ 
 	if ($metaInterval && ($metaPointer + $chunkSize) > $metaInterval) {
 
 		$chunkSize = $metaInterval - $metaPointer;
 
 		# This is very verbose...
 		#$log->debug("Reduced chunksize to $chunkSize for metadata");
-        Data::Dump::dump("Reduced chunksize for metadata", $chunkSize);
-        
-        $readLength = CORE::sysread($self, $_[1], $chunkSize, length($_[1] || '' ))
 
-    } else {
-        
-        $readLength = _sysread($self, $_[1], MIN_OUT);
-    }
-    
-    #Data::Dump::dump("sysread: ", $_[1], $chunkSize, length($_[1]));
-	#my $readLength = CORE::sysread($self, $_[1], $chunkSize, length($_[1] || '' ));
+		$readLength = CORE::sysread($self, $_[1], $chunkSize, length($_[1] || '' ))
 
+	} else {
+
+		$readLength = _sysread($self, $_[1], MIN_OUT);
+	}
+ 
 	if ($metaInterval && $readLength) {
 
 		$metaPointer += $readLength;
@@ -144,73 +144,73 @@ sub sysread {
 }
 
 sub _sysread(){
-    use bytes;
-    
-    my $self = $_[0];
-    
-    my $v = $self->vars;
-    my $url = ${*$self}{'url'};
+	use bytes;
 
-    # need more data
-	if ( length $v->{'outBuf'} < MAX_OUT && !$v->{'fetching'}) {
-        
-        my $range;
-        
-        if ($v->{'streaming'}){
-            $range = "bytes=$v->{offset}-" . ($v->{offset} + RANGE_SIZE - 1);
- 
-        } else {
-            
-            $range = "bytes=$v->{offset}-" . ($v->{offset} + 1);
-        }
-        $v->{'fetching'} = 1;
-        Data::Dump::dump("* Going to fetch:  ", $url, $range, length($v->{'inBuf'} || ''), $v->{'fetching'},$v->{'streaming'});
-						
+	my $self = $_[0];
+
+	my $client = $self->client;
+	my $maxOut = $client->bufferSize() || MAX_OUT;
+
+	my $v = $self->vars;
+	my $url = ${*$self}{'url'};
+
+	# need more data
+	if ( length $v->{'outBuf'} < $maxOut && !$v->{'fetching'}) {
+
+		my $range;
+
+		if ($v->{'streaming'}){
+
+			$range = "bytes=$v->{offset}-" . ($v->{offset} + RANGE_SIZE - 1);
+
+		} else {
+
+			$range = "bytes=$v->{offset}-" . ($v->{offset} + 1);
+		}
+
+		$v->{'fetching'} = 1;
+
+		#Data::Dump::dump("* Going to fetch: ", $url, $range, length($v->{'inBuf'} || ''), $v->{'fetching'},$v->{'streaming'});
+
 		Slim::Networking::SimpleAsyncHTTP->new(
 			sub {
 				$v->{'inBuf'} .= $_[0]->content;
 				$v->{'fetching'} = 0;
 				$v->{'streaming'} = 0 if length($_[0]->content) < RANGE_SIZE;
-                $v->{offset} += length($_[0]->content);
-                
+				$v->{offset} += length($_[0]->content);
+
 				main::DEBUGLOG && $log->is_debug && $log->debug("got chunk length: ", length $_[0]->content, " from ", $v->{offset} - RANGE_SIZE, " for $url");
-                Data::Dump::dump("* Got chunk length: ", length $_[0]->content, $v->{offset} - RANGE_SIZE, length $v->{'inBuf'});
-            },
-			
+			},
 			sub { 
 				$log->warn("error fetching $url");
 				$v->{'inBuf'} = '';
 				$v->{'fetching'} = 0;
-                Data::Dump::dump("error fetching $url");
 			}, 
-			
-		)->get($url, 'Range' => $range );
-		
-	}	
-    if (length $v->{'inBuf'} >= MIN_OUT || !$v->{'streaming'}){
-        
-        Data::Dump::dump("Going to feed OUT BUF:  ", length $v->{'inBuf'}, MIN_OUT);
-        
-        $v->{'outBuf'} = $v->{'outBuf'}.$v->{'inBuf'};
-        $v->{'inBuf'}='';
-    }
-    
-    my $bytes =length $v->{'outBuf'};
 
-    if ($bytes) {
-        Data::Dump::dump("Going to return OUT BUF:  ",$bytes, length $v->{'outBuf'});
+		)->get($url, 'Range' => $range );
+	}	
+	if (length $v->{'inBuf'} >= MIN_OUT || !$v->{'streaming'}){
+
+		$v->{'outBuf'} = $v->{'outBuf'}.$v->{'inBuf'};
+		$v->{'inBuf'}='';
+	}
+
+	my $bytes =length $v->{'outBuf'};
+
+	if ($bytes) {
+
 		$_[1] = $_[1].substr($v->{'outBuf'}, 0, $bytes);
 		$v->{'outBuf'} = substr($v->{'outBuf'}, $bytes);
 		return $bytes;
+
 	} elsif ( $v->{streaming} ) {
 		#$! = EINTR; Pipeline does not recocgnize EINTR;
-        $! = EWOULDBLOCK;
+		$! = EWOULDBLOCK;
 		return undef;
 	} else {
-        Data::Dump::dump("EOF");
-        return 0; #EOF.
-    }
-    
+		Data::Dump::dump("EOF");
+		return 0; #EOF.
+	}
 }
 
 sub canSeek { 0 }
@@ -258,9 +258,9 @@ sub parseHeaders {
 }
 
 sub parseDirectHeaders {
-	my $class   = shift;
-	my $client  = shift || return;
-	my $url     = shift;
+	my $class = shift;
+	my $client = shift || return;
+	my $url = shift;
 	my @headers = @_;
 	
 	# May get a track object
@@ -268,7 +268,7 @@ sub parseDirectHeaders {
 		$url = $url->url;
 	}
 	
-	my $bitrate     = 750_000;
+	my $bitrate = 750_000;
 	my $contentType = 'flc';
 	
 	my $length;
