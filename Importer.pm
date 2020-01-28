56bca534f1a0eb13df1c7fa36a07f123882e6087
Michael Herger <michael@herger.net>
2020-01-28 13:21:57 +0100

    Fail importer gracefully when run on LMS7

--- Importer.pm
+++ Importer.pm
@@ -2,7 +2,13 @@ package Plugins::Qobuz::Importer;
 
 use strict;
 
-use base qw(Slim::Plugin::OnlineLibraryBase);
+# can't "use base ()", as this would fail in LMS 7
+BEGIN {
+	eval {
+		require Slim::Plugin::OnlineLibraryBase;
+		our @ISA = qw(Slim::Plugin::OnlineLibraryBase);
+	};
+}
 
 use List::Util qw(max);
 
