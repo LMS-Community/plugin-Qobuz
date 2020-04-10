42cadd06c50843988ba13fb83195f722a1cbaed5
Michael Herger <michael@herger.net>
2020-04-10 18:11:05 +0200

    Replace Slim::Schema->rs('Contributor')->update_or_create() with Slim::Schema::Contributor->add(). The former would apply too much magic, triggering warnings and slowing things down. The latter is what is being used when adding tracks, too. Thus treating adding contributors directly the same as if they were added via a track.

--- Importer.pm
+++ Importer.pm
@@ -143,12 +143,10 @@ sub scanArtists {
 		$progress->update($name);
 		main::SCANNER && Slim::Schema->forceCommit;
 
-		Slim::Schema->rs('Contributor')->update_or_create({
-			'name'       => $name,
-			'namesort'   => Slim::Utils::Text::ignoreCaseArticles($name),
-			'namesearch' => Slim::Utils::Text::ignoreCase($name, 1),
-			'extid'      => 'qobuz:artist:' . $artist->{id},
-		}, { 'key' => 'namesearch' });
+		Slim::Schema::Contributor->add({
+			'artist' => $name,
+			'extid'  => 'qobuz:artist:' . $artist->{id},
+		});
 	}
 
 	$progress->final();
