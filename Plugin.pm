5aa29028865fdca8e49a02eae4646ea3ce16de5a
Sam Y <78810692+SamInPgh@users.noreply.github.com>
2025-01-07 12:42:56 -0500

    Update Plugin.pm

--- Plugin.pm
+++ Plugin.pm
@@ -1437,16 +1437,14 @@ sub QobuzGetTracks {
 		}
 
 		my @artistList = ();
-		my $numArtists = 0;
 		if (ref $album->{artists} && scalar @{$album->{artists}} ) {  # get all main artists
 			for my $artist ( @{$album->{artists}} ) {
 				if (grep(/main-artist/, @{$artist->{roles}})) {
 					push @artistList, $artist;
-					$numArtists++;
 				}
 			}
 		}
-		if (!$numArtists) {  # if no main artists found, use the primary artist
+		if (!scalar @artistList) {  # no main artists found, use the primary artist
 			push @artistList, $album->{artist};
 		}
 
